34fbf991ea542ca92a52ae473c6f5a56d27e7d15
João Paulo Pontes <joao.medeiros@gft.com>
2023-05-11 17:10:34 -0300

    refactor: change global styled-components to tailwind

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,29 +2,6 @@
 
 import { createGlobalStyle } from "styled-components"
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    font-family: 'DM Sans', Sans-Serif; 
-  }
-
-  body {
-    margin: 0 auto;
-    padding: 0;
-    height: 100vh;
-    max-width: calc(80%);
-  }
-
-  #root {
-    height: 100vh;
-  }
-
-  html {
-    height: 100vh;
-  }
-
-  h1,h2,h3,h4,h5 {
-    margin: 0;
-  }
-`
+const GlobalStyle = createGlobalStyle``
 
 export default GlobalStyle
